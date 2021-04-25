46c8ec5dd3716123ad54c05773a6c63b52422c2c
Paulo Teodoro <paulolimrs@outlook.com>
2021-04-25 14:29:43 -0300

    Aula 08-03-21

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -32,4 +32,47 @@ routes.delete('/produtos', (req, res) =>{
     return res.json({mensagem : 'Deleta o produto ' + id})
 }); 
 
+routes.get('/cliente', (req, res) =>{
+    return res.json({mensagem : "Rota de todos os Clientes"})
+});
+
+routes.post('/cliente', (req, res) =>{
+    var cliente = req.body.cliente;
+    return res.json({mensagem : 'Add cliente ' + cliente.nome})
+}); 
+
+routes.put('/cliente', (req, res) =>{
+    var id = req.query.id;
+    var cliente = req.body.cliente;
+    return res.json({mensagem : 'Atualizar o cliente ' + id + ' com os dados do post ' + cliente.nome})
+}); 
+
+routes.delete('/cliente', (req, res) =>{
+    var id = req.query.id;
+    var cliente = req.body.cliente;
+    return res.json({mensagem : 'Deleta o cliente ' + id + ', ' + cliente.nome})
+}); 
+
+routes.get('/pedido', (req, res) =>{
+    return res.json({mensagem : "Rota de todos os pedidos"})
+});
+
+routes.post('/pedido', (req, res) =>{
+    var id = req.query.id;
+    var pedido = req.body.pedido;
+    return res.json({mensagem : 'Add pedido ' + id})
+}); 
+
+routes.put('/pedido', (req, res) =>{
+    var id = req.query.id;
+    var pedido = req.body.pedido;
+    return res.json({mensagem : 'Atualizar o pedido ' + id + ' com os dados do post ' + pedido.nome})
+}); 
+
+routes.delete('/pedido', (req, res) =>{
+    var id = req.query.id;
+    var pedido = req.body.pedido;
+    return res.json({mensagem : 'Deleta o pedido ' + id + ', ' + pedido.nome})
+}); 
+
 module.exports = routes;
